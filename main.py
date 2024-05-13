d4eb7f97802b780940e6306e8b3466af1ed27c71
tseguim <tseguim@gmail.com>
2024-05-12 21:08:07 -0400

    imported turtle

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Hello World")
-print("Hola Mundo")
+import turtle
+
+
+turtle.color("yellow")
+turtle.Screen().bgcolor("black")
+
+turtle.width(12)
+
+for i in range(5):
+    turtle.forward(150)
+    turtle.right(144)
+
+turtle.done()
